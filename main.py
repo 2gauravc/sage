2d4725ce055a60d514d46d553bce711b1cfc88b7
gauravc <2gauravc@gmail.com>
2022-09-24 17:21:54 +0800

    Issue #10: Separate the table set-up input into a file

--- main.py
+++ main.py
@@ -3,18 +3,44 @@
 
 import os
 import openai
+import getopt, sys
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.api_key)
+def main(argv): 
+    try: 
+        opts, args = getopt.getopt(argv,"q:", ["query="])
+    except getopt.GetoptError:
+        print("Usage: python3 main.py --query=<query_path>")
+        sys.exit(2)
+    
+    req_options = 0
+    for opt, arg in opts:
+        if opt == '--query':
+            query_file = arg
+            req_options = 1
 
-response = openai.Completion.create(
-  model="code-davinci-002",
-  prompt="### Postgres SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n#\n### A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSELECT",
-  temperature=0,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=["#", ";"]
-)
-print (response)
+    if (req_options == 0):
+        print("Usage: python3 main.py --query=<query_path>")
+        sys.exit(2)
+    
+    ## Get the API key  
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    # Read the table structure 
+    with open(query_file) as f:
+        query_text = f.readlines()
+    print (query_text)
+    #sys.exit() 
+
+    response = openai.Completion.create(model="code-davinci-002",
+    prompt=query_text,
+    temperature=0,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,presence_penalty=0,
+    stop=["#", ";"]
+    )
+    print (response)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
